919e772a315bc3f13947e8db74c4b214d77e824f
husen-hn <hosseinspell@gmail.com>
2022-12-14 13:12:59 +0330

    init ⌁

---
+++ shopping-cart/app.js
@@ -0,0 +1,14 @@
+const cart = [];
+
+class Product {
+  getProducts() {}
+}
+
+class View {}
+
+class Storage {}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const view = new View();
+  const product = new Product();
+});
