7c1a8df21ed3b764ebf65fe7e4cf5167b16d6235
husen-hn <hosseinspell@gmail.com>
2023-01-18 10:26:59 +0330

    test route

--- juice-shopping-store/scripts/app.js
+++ juice-shopping-store/scripts/app.js
@@ -6,21 +6,28 @@
 
 // document.addEventListener('DOMContentLoaded', () => router.init())
 
+let contentDiv = document.getElementsByClassName('products')
+
 let routes = {
     '/': homepage,
     '/index.html': homepage,
-    '/portfolio': portfolio,
-    '/work': work,
     '/contact': contact
 }
 
 window.onpopstate = () => {
-    console.log('onpopstate')
+    console.log(`onpopstate ${window.location.pathname}`)
     contentDiv.innerHTML = routes[window.location.pathname]
 }
 
 let onNavItemClick = (pathName) => {
-    window.history.pushState({}, pathName, window.location.origin + pathName)
+    window.history.pushState(
+        {},
+        pathName,
+        window.location.origin +
+            // '/Front_End_challenges/juice-shopping-store' +
+            '/juice-shopping-store' +
+            pathName
+    )
     contentDiv.innerHTML = routes[pathName]
 }
 
