a9a716dff58d2616fe61e59f563f0c202e872ab6
husen-hn <hosseinspell@gmail.com>
2023-01-18 09:47:04 +0330

    test route

--- juice-shopping-store/scripts/app.js
+++ juice-shopping-store/scripts/app.js
@@ -18,3 +18,10 @@ window.onpopstate = () => {
     console.log('onpopstate')
     contentDiv.innerHTML = routes[window.location.pathname]
 }
+
+let onNavItemClick = (pathName) => {
+    window.history.pushState({}, pathName, window.location.origin + pathName)
+    contentDiv.innerHTML = routes[pathName]
+}
+
+contentDiv.innerHTML = routes[window.location.pathname]
