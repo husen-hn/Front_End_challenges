43f22e171b7fa0ebade68938a9738d2bcb243dd5
Hossein HassanNejad <hosseinspell@gmail.com>
2023-04-25 21:15:41 +0330

    react apps github page

--- react-first-steps/game-app/vite.config.ts
+++ react-first-steps/game-app/vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/Front_End_challenges/react-first-steps/game-app/',
     plugins: [react()]
 })
