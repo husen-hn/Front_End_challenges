f9dfb66859b44090df9fe66015d68025500094b6
Hossein HassanNejad <hosseinspell@gmail.com>
2023-05-14 10:52:03 +0330

    show loading skeletons

--- react-first-steps/game-hub/src/hooks/useGames.ts
+++ react-first-steps/game-hub/src/hooks/useGames.ts
@@ -24,21 +24,27 @@ interface FetchGamesResponse {
 const useGames = () => {
     const [games, setGames] = useState<Game[]>([])
     const [error, setError] = useState('')
+    const [isLodaing, setLoading] = useState(false)
 
+    setLoading(true)
     useEffect(() => {
         const controller = new AbortController()
         apiClient
             .get<FetchGamesResponse>('/games', { signal: controller.signal })
-            .then((res) => setGames(res.data.results))
+            .then((res) => {
+                setGames(res.data.results)
+                setLoading(false)
+            })
             .catch((err) => {
                 if (err instanceof CanceledError) return
                 setError(err.message)
+                setLoading(false)
             })
 
         return () => controller.abort()
     }, [])
 
-    return { games, error }
+    return { games, error, isLodaing }
 }
 
 export default useGames
