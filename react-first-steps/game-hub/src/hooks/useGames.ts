68d995fa091e535c2a03d46be94809442ec0833d
Hossein HassanNejad <hosseinspell@gmail.com>
2023-05-16 21:03:53 +0330

    sorting games

--- react-first-steps/game-hub/src/hooks/useGames.ts
+++ react-first-steps/game-hub/src/hooks/useGames.ts
@@ -21,10 +21,11 @@ const useGames = (gameQuery: GameQuery) =>
         {
             params: {
                 genres: gameQuery.genre?.id,
-                platforms: gameQuery.platform?.id
+                platforms: gameQuery.platform?.id,
+                ordering: gameQuery.sortOrder
             }
         },
-        [gameQuery.genre?.id, gameQuery.platform?.id]
+        [gameQuery]
     )
 
 export default useGames
