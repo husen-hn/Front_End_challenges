49b452c269e34936824a3e45a19518dc2acf6dd3
Hossein HassanNejad <hosseinspell@gmail.com>
2023-05-21 13:38:42 +0330

    build search input

--- react-first-steps/game-hub/src/components/NavBar.tsx
+++ react-first-steps/game-hub/src/components/NavBar.tsx
@@ -1,11 +1,13 @@
 import { HStack, Image } from '@chakra-ui/react'
 import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
+import SearhcInput from './SearhcInput'
 
 const NavBar = () => {
     return (
         <HStack padding="10px">
             <Image src={logo} boxSize="60px" />
+            <SearhcInput />
             <ColorModeSwitch />
         </HStack>
     )
