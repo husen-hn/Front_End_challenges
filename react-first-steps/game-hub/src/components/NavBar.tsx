f13088bee50da688068e2b5383b39f7f8702b0d2
Hossein HassanNejad <hosseinspell@gmail.com>
2023-05-21 20:17:06 +0330

    implement search

--- react-first-steps/game-hub/src/components/NavBar.tsx
+++ react-first-steps/game-hub/src/components/NavBar.tsx
@@ -3,11 +3,15 @@ import logo from '../assets/logo.webp'
 import ColorModeSwitch from './ColorModeSwitch'
 import SearhcInput from './SearhcInput'
 
-const NavBar = () => {
+interface Props {
+    onSearch: (searchText: string) => void
+}
+
+const NavBar = ({ onSearch }: Props) => {
     return (
         <HStack padding="10px">
             <Image src={logo} boxSize="60px" />
-            <SearhcInput />
+            <SearhcInput onSearch={onSearch} />
             <ColorModeSwitch />
         </HStack>
     )
--- react-first-steps/game-hub/src/components/SearhcInput.tsx
+++ react-first-steps/game-hub/src/components/SearhcInput.tsx
@@ -1,12 +1,30 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
+import { useRef } from 'react'
 import { BsSearch } from 'react-icons/bs'
 
-function SearhcInput() {
+interface Props {
+    onSearch: (searchText: string) => void
+}
+
+function SearhcInput({ onSearch }: Props) {
+    const ref = useRef<HTMLInputElement>(null)
+
     return (
-        <InputGroup>
-            <InputLeftElement children={<BsSearch />} />
-            <Input borderRadius={20} placeholder="Search games..."></Input>
-        </InputGroup>
+        <form
+            onSubmit={(event) => {
+                event.preventDefault()
+                if (ref.current) onSearch(ref.current.value)
+            }}
+        >
+            <InputGroup>
+                <InputLeftElement children={<BsSearch />} />
+                <Input
+                    ref={ref}
+                    borderRadius={20}
+                    placeholder="Search games..."
+                ></Input>
+            </InputGroup>
+        </form>
     )
 }
 
