49cba6aa91f4cd5f7bc80d39d4b93ec82b191ee5
Hossein HassanNejad <hosseinspell@gmail.com>
2023-05-28 21:24:57 +0330

    useEffect

--- react-first-steps/hook-tut/src/Hooks/useEffect/UseEffectHook.tsx
+++ react-first-steps/hook-tut/src/Hooks/useEffect/UseEffectHook.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
 function UseEffectHook() {
+    const [count, setCount] = useState(0)
+
+    useEffect(() => {
+        setTimeout(() => {
+            setCount((count) => count + 1)
+        }, 3000)
+    })
+
     return (
-        <div style={{ padding: 20 }}>
-            <h2>About View</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adip.</p>
+        <div className="p-20 text-xl font-bold">
+            I've rendered {count} times!
         </div>
     )
 }
