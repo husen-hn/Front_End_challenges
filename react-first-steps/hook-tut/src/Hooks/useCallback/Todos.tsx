cf672813c97e01be5188f44c98e2ee2cf8aefc57
Hossein HassanNejad <hosseinspell@gmail.com>
2023-06-02 13:02:44 +0330

    useCallback

--- react-first-steps/hook-tut/src/Hooks/useCallback/Todos.tsx
+++ react-first-steps/hook-tut/src/Hooks/useCallback/Todos.tsx
@@ -6,14 +6,25 @@ interface Props {
 }
 
 function Todos({ todos, addTodo }: Props) {
-    console.log('child render')
     return (
         <>
-            <h2>My Todos</h2>
-            {todos.map((todo: string, index: number) => {
-                return <p key={index}>{todo}</p>
+            <h2 className="text-3xl font-bold m-10 ">My Todos</h2>
+            {todos.map((todo, index) => {
+                return (
+                    <p
+                        className="text-xl text-gray-400 font-semibold ml-10 mb-3"
+                        key={index}
+                    >
+                        {todo}
+                    </p>
+                )
             })}
-            <button onClick={addTodo}>Add Todo</button>
+            <button
+                className="shadow bg-[#F09383] hover:bg-[#FAC29A] focus:shadow-outline focus:outline-none text-black font-bold py-2 px-4 m-10 rounded"
+                onClick={addTodo}
+            >
+                Add Todo
+            </button>
         </>
     )
 }
--- react-first-steps/hook-tut/src/Hooks/useCallback/UseCallbackHook.tsx
+++ react-first-steps/hook-tut/src/Hooks/useCallback/UseCallbackHook.tsx
@@ -2,6 +2,7 @@ import { useCallback, useState } from 'react'
 import Todos from './Todos'
 
 function UseCallbackHook() {
+    console.log('UseCallbackHook child render')
     const [count, setCount] = useState(0)
     const [todos, setTodos] = useState<string[]>([])
 
@@ -12,15 +13,15 @@ function UseCallbackHook() {
     const addTodo = useCallback(() => {
         setTodos((t) => [...t, 'New Todo'])
     }, [todos])
+
     return (
         <>
             <Todos todos={todos} addTodo={addTodo} />
             <hr />
-            <div className="text-xl font-bold">
+            <div className="text-xl font-bold text-[#F09383]">
                 Count: {count}
                 <button
-                    type="button"
-                    className="p-3 border-double border-2 border-[#09F7A0] rounded-xl m-5"
+                    className="p-5 border-double border-2 border-[#09F7A0] rounded-xl m-5 text-[#09F7A0]"
                     onClick={increment}
                 >
                     +
